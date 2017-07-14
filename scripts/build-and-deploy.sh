ee94be303581eed1e201f8445133929acb3d0a0e
Brian Peiris <brianpeiris@gmail.com>
2017-07-13 22:54:01 -0700

    fix deploy

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,4 +1,4 @@
 bundle exec jekyll build
 expires=$(date -d 'next year' -uR)
-aws s3 sync _site s3://$DEV_SITE_BUCKET --recursive --acl public-read --expires "$expires"
+aws s3 sync _site s3://$DEV_SITE_BUCKET --acl public-read --expires "$expires"
 aws create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '*'
