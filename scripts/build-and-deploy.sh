6a89af8b2f4cf465a02fe44d8a5b8a3a3244557e
Brian Peiris <brianpeiris@gmail.com>
2017-07-13 23:06:25 -0700

    fix invalidation

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,4 +1,5 @@
 bundle exec jekyll build
 expires=$(date -d 'next year' -uR)
 aws s3 sync _site s3://$DEV_SITE_BUCKET --acl public-read --expires "$expires"
+aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '*'
