d5a6a1b20855ed8cf2ffdacb2de6db5840304dbf
Brian Peiris <brianpeiris@gmail.com>
2017-07-17 13:04:22 -0700

    fix deploy

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -6,6 +6,6 @@ aws s3 sync _site s3://$DEV_SITE_BUCKET --acl public-read --expires "$expires"
 aws configure set preview.cloudfront true
 
 result=$(aws cloudfront create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '/*')
-invalidation_id=$($result | grep '"Id":' | cut -d'"' -f4)
+invalidation_id=$(echo "$result" | grep '"Id":' | cut -d'"' -f4)
 
 aws cloudfront wait invalidation-completed --distribution-id $DEV_SITE_DISTRIBUTION --id $invalidation_id
