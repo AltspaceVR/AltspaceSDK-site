a5a218a15f81a5021f508d3c9274e0ebd830cd43
Brian Peiris <brianpeiris@gmail.com>
2017-07-17 14:05:56 -0700

    fix caching and notification

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,11 +1,2 @@
-bundle install
+bundle install --verbose
 pip install --user awscli
-
-du -hd0 vendor
-
-echo 'local/lib'
-ls ~/.local/lib/
-ls ~/.local/lib/py*
-
-echo 'local/bin'
-ls ~/.local/bin/
