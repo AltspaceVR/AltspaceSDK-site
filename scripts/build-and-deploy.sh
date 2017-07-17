711a47f5eabccde2ffb574b19a593570dbccdcda
Brian Peiris <brianpeiris@gmail.com>
2017-07-17 12:59:34 -0700

    wait for invalidation and add message

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,6 +1,11 @@
 bundle exec jekyll build
+
 expires=$(date -d 'next year' -uR)
 aws s3 sync _site s3://$DEV_SITE_BUCKET --acl public-read --expires "$expires"
+
 aws configure set preview.cloudfront true
-echo $DEV_SITE_DISTRIBUTION
-aws cloudfront create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '/*'
+
+result=$(aws cloudfront create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '/*')
+invalidation_id=$($result | grep '"Id":' | cut -d'"' -f4)
+
+aws cloudfront wait invalidation-completed --distribution-id $DEV_SITE_DISTRIBUTION --id $invalidation_id
