a4899775f5d046913841d57aeb601d346b59f3c0
Steven Vergenz <Steven.Vergenz@microsoft.com>
2017-10-05 15:13:11 -0700

    Fix deploy

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,16 +1,15 @@
 echo 'building site'
 bundle exec jekyll build
 
-export AWS="$HOME/.local/lib/python2.7/scripts/aws"
 echo 'uploading'
 expires=$(date -d 'next year' -uR)
-"$AWS" s3 sync _site s3://$DEV_SITE_BUCKET --acl public-read --expires "$expires"
+python -m awscli s3 sync _site s3://$DEV_SITE_BUCKET --acl public-read --expires "$expires"
 
-"$AWS" configure set preview.cloudfront true
+python -m awscli configure set preview.cloudfront true
 
 echo 'invalidating cache'
-result=$("$AWS" cloudfront create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '/*')
+result=$(python -m awscli cloudfront create-invalidation --distribution-id $DEV_SITE_DISTRIBUTION --paths '/*')
 invalidation_id=$(echo "$result" | grep '"Id":' | cut -d'"' -f4)
 
 echo 'waiting for invalidation'
-"$AWS" cloudfront wait invalidation-completed --distribution-id $DEV_SITE_DISTRIBUTION --id $invalidation_id
+python -m awscli cloudfront wait invalidation-completed --distribution-id $DEV_SITE_DISTRIBUTION --id $invalidation_id
